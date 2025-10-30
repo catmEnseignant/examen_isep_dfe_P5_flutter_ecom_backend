99ec2bde3ad150250882694abba64caad7d71290
catm2017 <cheikhmbengue146@gmail.com>
2025-10-30 09:49:33 +0000

    Test protect main

--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,4 @@ Route::post('/categories/store', [\App\Http\Controllers\API\CategorieAPIControll
 
 Route::put('/categories/update/{id_categorie}', [\App\Http\Controllers\API\CategorieAPIController::class,'update']);
 
-//Pour Produits s'inspirer des routes de api
+//Pour Produits s'inspirer des routes de api créer les controllers
